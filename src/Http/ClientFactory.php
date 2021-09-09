95ddecb2503134feadb09a1a581a2fd9b00f8cd0
bzzhh <sjehann@gmail.com>
2021-09-10 00:12:20 +0200

    lint

--- src/Http/ClientFactory.php
+++ src/Http/ClientFactory.php
@@ -7,12 +7,8 @@ namespace Bzzhh\Pezos\Http;
 use Bzzhh\Pezos\Generated\Mempool\Client as MempoolClient;
 use Bzzhh\Pezos\Generated\Proto\Client as ProtoClient;
 use Bzzhh\Pezos\Generated\Shell\Client as ShellClient;
-use Http\Client\Common\Plugin;
-use Http\Client\Common\Plugin\AddHostPlugin;
-use Http\Client\Common\Plugin\AddPathPlugin;
 use Http\Client\Common\PluginClient;
 use Http\Discovery\HttpClientDiscovery;
-use Http\Discovery\Psr17FactoryDiscovery;
 
 class ClientFactory
 {
